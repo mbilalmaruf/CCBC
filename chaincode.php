4c7bbded7d2b385a578f8433cd7a6b0a3b2afb37
mbilalmaruf <mbilalmaruf@gmail.com>
2017-04-09 02:18:35 +0500

    added by Billo

---
+++ chaincode.php
@@ -0,0 +1,29 @@
+<?php
+        
+		
+		
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+  $data = json_decode(file_get_contents("php://input"));
+  //echo json_encode($data);
+}
+	
+	
+$data_string = json_encode($data);
+
+$ch = curl_init('http://125.209.114.195:7050/chaincode');
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($data_string))
+);
+
+$result = curl_exec($ch);
+
+header('Content-Type: application/json');
+echo $result;
+
+	
+?>
